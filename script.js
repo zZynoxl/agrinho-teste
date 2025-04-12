0eebddc2c5104f7784c5afa5a4e4ea62ee4c451e
zZynoxl <santos.francesco@escola.pr.gov.br>
2025-04-11 22:27:39 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-.
+document.addEventListener('DOMContentLoaded', function () {
+    let tamanhoAtualFonte = 1;
+
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+
+    aumentaFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
+
+    const  diminuirFonteBotao = document.getElementById ('diminuir-fonte');
+
+    diminuirFonteBotao.addEventListener ('click', function (){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
+    
+    const alternaContraste = document.getElementById('alterna-contraste')
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
+    })
+})
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+    })
